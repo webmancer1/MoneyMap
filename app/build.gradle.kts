61ba1c16b2f6c48371ab367ca7abc6b26d5cd492
webmancer1 <mwendwajames2004@gmail.com>
2026-01-06 00:42:07 +0300

    gradle changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,6 +19,7 @@ android {
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
+        multiDexEnabled = true
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
