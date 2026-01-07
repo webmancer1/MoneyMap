230a0b622d7018843a0ec452ba1ee7785fca9c78
webmancer1 <mwendwajames2004@gmail.com>
2026-01-07 09:07:09 +0300

    UI changes

--- app/src/main/java/com/example/moneymap/ui/theme/Color.kt
+++ app/src/main/java/com/example/moneymap/ui/theme/Color.kt
@@ -3,61 +3,62 @@ package com.example.moneymap.ui.theme
 import androidx.compose.ui.graphics.Color
 
 // Light scheme
-val PrimaryGreen40 = Color(0xFF006C4D)
-val OnPrimaryGreen40 = Color(0xFFFFFFFF)
-val PrimaryGreenContainer40 = Color(0xFF8BF8C8)
-val OnPrimaryGreenContainer40 = Color(0xFF002116)
+// Light scheme
+val PrimaryNavy40 = Color(0xFF1B3B6F)
+val OnPrimaryNavy40 = Color(0xFFFFFFFF)
+val PrimaryNavyContainer40 = Color(0xFFD3E3FF)
+val OnPrimaryNavyContainer40 = Color(0xFF001C3A)
 
-val Sage40 = Color(0xFF4E6355)
-val OnSage40 = Color(0xFFFFFFFF)
-val SageContainer40 = Color(0xFFD0E8D5)
-val OnSageContainer40 = Color(0xFF0B1F15)
+val Gold40 = Color(0xFF755B00)
+val OnGold40 = Color(0xFFFFFFFF)
+val GoldContainer40 = Color(0xFFFFDF9E)
+val OnGoldContainer40 = Color(0xFF251A00)
 
-val Ocean40 = Color(0xFF39656F)
-val OnOcean40 = Color(0xFFFFFFFF)
-val OceanContainer40 = Color(0xFFBDEAF6)
-val OnOceanContainer40 = Color(0xFF001F27)
+val Slate40 = Color(0xFF525F70)
+val OnSlate40 = Color(0xFFFFFFFF)
+val SlateContainer40 = Color(0xFFD6E3F7)
+val OnSlateContainer40 = Color(0xFF0F1C2B)
 
 val ErrorLight = Color(0xFFBA1A1A)
 val OnErrorLight = Color(0xFFFFFFFF)
 val ErrorContainerLight = Color(0xFFFFDAD6)
 val OnErrorContainerLight = Color(0xFF410002)
 
-val BackgroundLight = Color(0xFFFBFDF7)
-val OnBackgroundLight = Color(0xFF191C1A)
+val BackgroundLight = Color(0xFFFDFBFF)
+val OnBackgroundLight = Color(0xFF1A1C1E)
 val SurfaceLight = BackgroundLight
 val OnSurfaceLight = OnBackgroundLight
-val SurfaceVariantLight = Color(0xFFDCE5DB)
-val OnSurfaceVariantLight = Color(0xFF404942)
-val OutlineLight = Color(0xFF707972)
-val OutlineVariantLight = Color(0xFFC0C9C0)
+val SurfaceVariantLight = Color(0xFFE0E2EC)
+val OnSurfaceVariantLight = Color(0xFF44474E)
+val OutlineLight = Color(0xFF74777F)
+val OutlineVariantLight = Color(0xFFC4C6D0)
 
 // Dark scheme
-val PrimaryGreen80 = Color(0xFF6DDBAD)
-val OnPrimaryGreen80 = Color(0xFF003824)
-val PrimaryGreenContainer80 = Color(0xFF005236)
-val OnPrimaryGreenContainer80 = Color(0xFF8BF8C8)
+val PrimaryNavy80 = Color(0xFFAEC6FF)
+val OnPrimaryNavy80 = Color(0xFF002E5C)
+val PrimaryNavyContainer80 = Color(0xFF004480)
+val OnPrimaryNavyContainer80 = Color(0xFFD3E3FF)
 
-val Sage80 = Color(0xFFB4CCBA)
-val OnSage80 = Color(0xFF213428)
-val SageContainer80 = Color(0xFF374B3E)
-val OnSageContainer80 = Color(0xFFD0E8D5)
+val Gold80 = Color(0xFFF3C53C)
+val OnGold80 = Color(0xFF3E2F00)
+val GoldContainer80 = Color(0xFF594400)
+val OnGoldContainer80 = Color(0xFFFFDF9E)
 
-val Ocean80 = Color(0xFFA2CEDA)
-val OnOcean80 = Color(0xFF033541)
-val OceanContainer80 = Color(0xFF214C55)
-val OnOceanContainer80 = Color(0xFFBDEAF6)
+val Slate80 = Color(0xFFBAC8DB)
+val OnSlate80 = Color(0xFF243140)
+val SlateContainer80 = Color(0xFF3B4858)
+val OnSlateContainer80 = Color(0xFFD6E3F7)
 
 val ErrorDark = Color(0xFFFFB4AB)
 val OnErrorDark = Color(0xFF690005)
 val ErrorContainerDark = Color(0xFF93000A)
 val OnErrorContainerDark = Color(0xFFFFDAD6)
 
-val BackgroundDark = Color(0xFF101410)
-val OnBackgroundDark = Color(0xFFE0E3DD)
+val BackgroundDark = Color(0xFF1A1C1E)
+val OnBackgroundDark = Color(0xFFE3E2E6)
 val SurfaceDark = BackgroundDark
 val OnSurfaceDark = OnBackgroundDark
-val SurfaceVariantDark = Color(0xFF404942)
-val OnSurfaceVariantDark = Color(0xFFC0C9C0)
-val OutlineDark = Color(0xFF8A938B)
+val SurfaceVariantDark = Color(0xFF44474E)
+val OnSurfaceVariantDark = Color(0xFFC4C6D0)
+val OutlineDark = Color(0xFF8E9099)
 val OutlineVariantDark = SurfaceVariantDark
